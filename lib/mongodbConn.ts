cead29fcc368d3c462cbef3d5078996eb3a7b5b7
AakrshitThakur <thakurraakrshitt@gmail.com>
2025-06-13 19:34:27 +0530

    Removed useNewUrlParser and useUnifiedTopology for options which are deprecated now

--- lib/mongodbConn.ts
+++ lib/mongodbConn.ts
@@ -13,34 +13,27 @@ interface MongooseGlobal {
 
 declare global {
   // Ensure we're not redefining in a hot-reload scenario
-  // @ts-ignore
   var mongooseGlobal: MongooseGlobal;
 }
 
 // Initialize global variable if it doesn't exist
-// @ts-ignore
 global.mongooseGlobal ||= {
   conn: null,
   promise: null,
 };
 
 async function dbConnect() {
-  // @ts-ignore
+
   if (global.mongooseGlobal.conn) return global.mongooseGlobal.conn;
 
-  // @ts-ignore
   if (!global.mongooseGlobal.promise) {
-    // @ts-ignore
     global.mongooseGlobal.promise = mongoose.connect(MONGODB_URI!, {
       bufferCommands: false,
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
     });
   }
 
   // @ts-ignore
   global.mongooseGlobal.conn = await global.mongooseGlobal.promise;
-  // @ts-ignore
   return global.mongooseGlobal.conn;
 }
 
