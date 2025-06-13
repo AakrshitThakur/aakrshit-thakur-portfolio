744576b993b1db3d33c00c74699fa2fc7f9de999
AakrshitThakur <thakurraakrshitt@gmail.com>
2025-06-13 19:17:55 +0530

    Delete the cached variable for type errors

--- lib/mongodbConn.ts
+++ lib/mongodbConn.ts
@@ -1,42 +1,47 @@
 import mongoose from "mongoose";
 
-declare global {
-  var mongoose: {
-    conn: any;
-    promise: Promise<any> | null;
-  };
-}
-
 const MONGODB_URI = process.env.MONGODB_URI;
 
 if (!MONGODB_URI) {
   throw new Error("Please define the MONGODB_URI environment variable");
 }
 
-let cached = global.mongoose;
-
-if (!cached) {
-  cached = {
-    conn: null,
-    promise: null,
-  };
-  global.mongoose = cached;
+interface MongooseGlobal {
+  conn: typeof mongoose | null;
+  promise: Promise<typeof mongoose> | null;
 }
 
-// Now cached is guaranteed to be defined
-async function dbConnect() {
-  if (cached!.conn) return cached!.conn;
+declare global {
+  // Ensure we're not redefining in a hot-reload scenario
+  // @ts-ignore
+  var mongooseGlobal: MongooseGlobal;
+}
 
-  if (!cached!.promise) {
-    cached!.promise = mongoose.connect(MONGODB_URI!, {
+// Initialize global variable if it doesn't exist
+// @ts-ignore
+global.mongooseGlobal ||= {
+  conn: null,
+  promise: null,
+};
+
+async function dbConnect() {
+  // @ts-ignore
+  if (global.mongooseGlobal.conn) return global.mongooseGlobal.conn;
+
+  // @ts-ignore
+  if (!global.mongooseGlobal.promise) {
+    // @ts-ignore
+    global.mongooseGlobal.promise = mongoose.connect(MONGODB_URI!, {
       bufferCommands: false,
       useNewUrlParser: true,
       useUnifiedTopology: true,
-    }).then((mongoose) => mongoose);
+    });
   }
 
-  cached!.conn = await cached!.promise;
-  return cached!.conn;
+  // @ts-ignore
+  global.mongooseGlobal.conn = await global.mongooseGlobal.promise;
+  // @ts-ignore
+  return global.mongooseGlobal.conn;
 }
 
 export default dbConnect;
