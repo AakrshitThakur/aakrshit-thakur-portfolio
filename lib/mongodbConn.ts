39b8d6718a3cbedd12ebd97d58151d2d8b175343
AakrshitThakur <thakurraakrshitt@gmail.com>
2025-06-13 19:00:46 +0530

    cached type error resolved for TypeScript

--- lib/mongodbConn.ts
+++ lib/mongodbConn.ts
@@ -1,5 +1,11 @@
-// lib/mongoose.js
 import mongoose from "mongoose";
+
+declare global {
+  var mongoose: {
+    conn: any;
+    promise: Promise<any> | null;
+  } | undefined;
+}
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
@@ -7,29 +13,30 @@ if (!MONGODB_URI) {
   throw new Error("Please define the MONGODB_URI environment variable");
 }
 
-declare global {
-  var mongoose: { conn: any; promise: any } | undefined;
-}
-
 let cached = global.mongoose;
 
 if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null };
+  cached = {
+    conn: null,
+    promise: null,
+  };
+  global.mongoose = cached;
 }
 
+// Now cached is guaranteed to be defined
 async function dbConnect() {
   if (cached!.conn) return cached!.conn;
 
-  if (!cached.promise) {
-    cached.promise = mongoose.connect(MONGODB_URI, {
+  if (!cached!.promise) {
+    cached!.promise = mongoose.connect(MONGODB_URI!, {
       bufferCommands: false,
       useNewUrlParser: true,
       useUnifiedTopology: true,
     }).then((mongoose) => mongoose);
   }
 
-  cached.conn = await cached.promise;
-  return cached.conn;
+  cached!.conn = await cached!.promise;
+  return cached!.conn;
 }
 
 export default dbConnect;
